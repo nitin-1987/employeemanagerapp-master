892025d51fae1206bbfb965811b059d3eca8fab9
nitin-1987 <njsingla333@gmail.com>
2021-03-14 11:56:17 +0000

    onInit

--- src/app/app-start/app-start.component.ts
+++ src/app/app-start/app-start.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnChanges, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-app-start',
   templateUrl: './app-start.component.html',
   styleUrls: ['./app-start.component.css']
 })
-export class AppStartComponent implements OnInit {
+export class AppStartComponent implements OnInit, OnChanges {
+
 
   constructor() { }
 
+
   ngOnInit(): void {
+  }
+  ngOnChanges(): void {
   }
 
 }
